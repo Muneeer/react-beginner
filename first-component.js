353b990e5636780cea85ac1fbbe176d327414833
Muneeer <muneerabbas50@gmail.com>
2021-05-25 16:59:34 +0900

    first component

--- first-component.js
+++ first-component.js
@@ -2,7 +2,7 @@ import React from 'react' //ES6 module
 import ReactDom from 'react-dom'
 
 function Greeting(){ //always capital to let react knows that this is not a function and its a component
-  return <h4>This is Muneer and this is my first component</h4> //must return
-} //Component created
+  return <h4>This is Muneer and this is my first component</h4>
+} 
  //adding component to our index HTML
  ReactDom.render(<Greeting/>,document.getElementById('root'))
